819d7ca054796a9423fa8b1c1577c278a3fc5aae
sdpercy <scottpercy@hotmail.com>
2021-07-25 14:47:40 -0230

    added javascript fpr add, delete, edit, login, logout

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 
-const { Product, User } = require('../models');
 
+
+router.get('/', (req, res) => {
+    res.render('homepage');
+  });
 
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
@@ -10,6 +13,15 @@ router.get('/login', (req, res) => {
     }
   
     res.render('login');
+  });
+
+  router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('signup');
   });
   
   module.exports = router;
