8ae1d8648fd58eccc24a6dd85ec30a0ccd0efdab
lisahuele <lisahuele@gmail.com>
2021-07-25 20:35:54 -0400

    signup/signin modal

--- src/js/script.js
+++ src/js/script.js
@@ -8,3 +8,57 @@ $(document).scroll(function(){
         }
     });
 });
+
+
+$(function() {
+    var overlay = $('.overlay');
+    var mainPopUp = $('.main-popup')
+    var signIn = $('#sign-in');
+    var register = $('#register');
+    var formSignIn = $('form.sign-in');
+    var formRegister = $('form.register');
+
+
+    $('.login').on('click', function() {
+        overlay.addClass('visible');
+        mainPopUp.addClass('visible');
+        signIn.addClass('active');
+        register.removeClass('active');
+        formRegister.removeClass('move-left');
+        formSignIn.removeClass('move-left');
+    });
+
+    $('.signup').on('click', function() {
+        overlay.addClass('visible');
+        mainPopUp.addClass('visible');
+        signIn.removeClass('active');
+        register.addClass('active');
+        formRegister.addClass('move-left');
+        formSignIn.addClass('move-left');
+    });
+
+    overlay.on('click', function(){
+        $(this).removeClass('visible');
+        mainPopUp.removeClass('visible');
+    });
+
+    $('#popup-close-button a').on('click', function(e){
+        e.preventDefault();
+        overlay.removeClass('visible');
+        mainPopUp.removeClass('visible');
+    });
+    
+    signIn.on('click', function(){
+        signIn.addClass('active');
+        register.removeClass('active');
+        formSignIn.removeClass('move-left');
+        formRegister.removeClass('move-left');
+    });
+    
+    register.on('click', function(){
+        signIn.removeClass('active');
+        register.addClass('active');
+        formSignIn.addClass('move-left');
+        formRegister.addClass('move-left');
+    });
+});
